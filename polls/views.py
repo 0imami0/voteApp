0dcb9f57a83f1e5ad44ee50593408d828cf056a9
0imami0 <000ima.ami333@gmail.com>
2021-07-28 01:06:28 -0500

    only results view left

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
 from .models import Question, Choice
 
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+
 def index(request):
-	return render(request, 'polls/index.html')
+	questions = Question.objects.order_by('-pub_date')[:6]
+	context = {'questions':questions}
+	return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+	question = Question.objects.get(id=question_id)
+	context = {'question':question}
+	return render(request, 'polls/detail.html', context)
+
+
+def vote(request, question_id):
+	question = Question.objects.get(pk=question_id)
+	try:
+		choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question':question, 
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		choice.votes += 1
+		choice.save()
+		# context = {'question':question, 'choice':choice}
+		# return render(request, 'polls/results.html', context)
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+def results(request, question_id):
+	question = Question.objects.get(pk=question_id)
+	context = {'question':question}
+	return render(request, 'polls/results.html' , context)
